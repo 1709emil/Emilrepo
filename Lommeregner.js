981c7297255e96b57a192ef842d207681610f833
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2020-11-02 10:20:39 +0100

    først del af lommeregneren

--- Lommeregner.js
+++ Lommeregner.js
@@ -1,1 +1,14 @@
-
+function udregning(x){
+let n1 = parseInt(document.getElementById("textBoks1").value);
+let n2 = parseInt(document.getElementById("textBoks2").value);
+let result;
+if (x === "+")
+result = n1+n2;
+else if (x === "-")
+result = n1-n2;
+else if (x === "*")
+result = n1*n2;
+else if (x === "/")
+result = n1/n2;
+document.getElementById("resultat").value = result;
+}

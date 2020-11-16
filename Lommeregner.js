fecab8d6f7723815020e0373c891ad9c1d68b5dc
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2020-11-16 10:34:59 +0100

    Potens

--- Lommeregner.js
+++ Lommeregner.js
@@ -12,3 +12,19 @@ else if (x === "/")
 result = n1/n2;
 document.getElementById("resultat").value = result;
 }
+
+function potAndsqrt(y){
+let n3 = parseInt(document.getElementById('textboks3').value);
+let n4 = parseInt(document.getElementById('textboks4').value);
+let answer=1 ;
+if (y=== "pot") {
+for (let i=0; i < n4; i++ ){
+  answer = answer * n3
+}
+document.getElementById("svar").value = answer;
+return answer
+}
+else if (y ==="√") {
+}
+
+}
